a301e242970165d96c71cb78c20c525759aaac65
Julie Tran <juliee.trann@gmail.com>
2021-02-11 23:51:59 -0700

    removed untracked changes

--- Term-2-Travel-Experts-Project--main/Packages_Products_SuppliersData/Packages_Products_Suppliers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Packages_Products_SuppliersData
-{
-    public class Packages_Products_Suppliers
-    {
-        public int PackageId { get; set; }
-        public int ProductSupplerId { get; set; }
-    }
-}
